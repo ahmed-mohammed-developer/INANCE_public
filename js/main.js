4035db13bd11cf81511fa1fb4f994abb8f669451
ahmed <ahmed.mohammed.developer@gmail.com>
2024-03-07 14:29:33 +0300

    Number js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 
 let nums = document.querySelectorAll(".number-cont .num");
 let section = document.querySelector(".numbers");
-let started = false; // Function Started ? No
-
-
+let started = false;
 window.onscroll = function () {
      if (window.scrollY >= section.offsetTop){
-          if (!started) {
-               number-cont552.forEach((num) => startCount(num));
+          if(!started) {
+               nums.forEach((num) => startCount(num));
           }
           started = true;
      }
 }
-
 function startCount(el) {
      let goal = el.dataset.goal;
      let count = setInterval(() => {
           el.textContent++;
-          if (el.textContent == goal) {
+          if (el.textContent == goal){
                clearInterval(count)
           }
      }, 2000 / goal);
+    
 }
-
+//startCount(document.querySelectorAll(".number-cont .num")[0]);
+// Number Section
